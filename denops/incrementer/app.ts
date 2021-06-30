14c510ef3efa5d551d2d6c1af9224e96e49e9071
0ta2 <mangamania5643+github@gmail.com>
2021-06-30 23:04:22 +0900

    Upgrade Denops

--- denops/incrementer/app.ts
+++ denops/incrementer/app.ts
@@ -1,34 +1,38 @@
-import { main } from "./deps.ts";
+import type { Denops } from "./deps.ts";
+import { execute } from "./deps.ts";
 
-main(async ({ vim }) => {
-  vim.register({
+export async function main(denops: Denops): Promise<void> {
+  denops.dispatcher = {
     async increment(): Promise<void> {
-      const cword = await vim.call("expand", "<cword>") as number;
+      const cword = await denops.call("expand", "<cword>") as number;
 
       const num = Number(cword) + 1;
 
-      await vim.cmd(`let @a = "${num}"`);
-      await vim.cmd('normal "_diw');
-      await vim.cmd('normal "aP');
+      await denops.cmd(`let @a = "${num}"`);
+      await denops.cmd('normal "_diw');
+      await denops.cmd('normal "aP');
 
       console.log(num);
     },
 
     async decrement(): Promise<void> {
-      const cword = await vim.call("expand", "<cword>") as number;
+      const cword = await denops.call("expand", "<cword>") as number;
 
       const num = Number(cword) - 1;
 
-      await vim.cmd(`let @a = "${num}"`);
-      await vim.cmd('normal "_diw');
-      await vim.cmd('normal "aP');
+      await denops.cmd(`let @a = "${num}"`);
+      await denops.cmd('normal "_diw');
+      await denops.cmd('normal "aP');
 
       console.log(num);
     },
-  });
+  };
 
-  await vim.execute(`
-    command! Incrementer echomsg denops#request('${vim.name}', 'increment', []),
-    command! Decrement echomsg denops#request('${vim.name}', 'decrement', [])
-  `);
-});
+  await execute(
+    denops,
+    `
+    command! Incrementer echomsg denops#request('${denops.name}', 'increment', []),
+    command! Decrement echomsg denops#request('${denops.name}', 'decrement', [])
+    `,
+  );
+}
--- denops/incrementer/deps.ts
+++ denops/incrementer/deps.ts
@@ -1,1 +1,2 @@
-export { main, Vim } from "https://deno.land/x/denops_std@v0.15.0/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/helper/mod.ts";
