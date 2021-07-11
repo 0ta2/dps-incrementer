d9376c937869fab1c14ee726a8df87d0eff357a5
0ta2 <mangamania5643+github@gmail.com>
2021-07-11 12:35:08 +0900

    Refactoring

--- denops/incrementer/deps.ts
+++ denops/incrementer/deps.ts
@@ -1,2 +1,3 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-alpha.0/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.1/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v1.0.0-beta.1/function/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.1/helper/mod.ts";
--- denops/incrementer/main.ts
+++ denops/incrementer/main.ts
@@ -1,34 +1,97 @@
 import type { Denops } from "./deps.ts";
 import { execute } from "./deps.ts";
 import { cword } from "./cword.ts";
+import { write } from "./write.ts";
+import { weeks } from "./values/weeks.ts";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     async increment(): Promise<void> {
-      const word = await cword(denops);
-
+      const word: string = await cword(denops);
       let num = Number(word);
-      if (typeof num === "number") {
+
+      if (isNaN(num)) {
+        if (weeks.includes(word)) {
+          const weekIndex = weeks.indexOf(word);
+          if (weeks.length === weekIndex + 1) {
+            const week = weeks[0];
+
+            await denops.cmd(`let @a = "${week}"`);
+            await denops.cmd('normal "_diw');
+
+            const col = await denops.call("col", ".") as number;
+            const currentLineText = await denops.call("getline", ".") as string;
+
+            if (currentLineText[col - 1] === " ") {
+              await denops.cmd('normal "ap');
+            } else {
+              await denops.cmd('normal "aP');
+            }
+          } else {
+            const week = weeks[weekIndex + 1];
+
+            await denops.cmd(`let @a = "${week}"`);
+            await denops.cmd('normal "_diw');
+
+            const col = await denops.call("col", ".") as number;
+            const currentLineText = await denops.call("getline", ".") as string;
+
+            console.log(currentLineText);
+            if (currentLineText[col - 1] === " ") {
+              await denops.cmd('normal "ap');
+            } else {
+              await denops.cmd('normal "aP');
+            }
+          }
+        }
+      } else {
         ++num;
-        await denops.cmd(`let @a = "${num}"`);
-        await denops.cmd('normal "_diw');
-        await denops.cmd('normal "aP');
+        write(denops, num);
       }
     },
 
     async decrement(): Promise<void> {
-      const word = await cword(denops);
-
+      const word: string = await cword(denops);
       let num = Number(word);
 
-      if (typeof num === "number") {
-        --num;
-        await denops.cmd(`let @a = "${num}"`);
-        await denops.cmd('normal "_diw');
-        await denops.cmd('normal "aP');
-      }
+      if (isNaN(num)) {
+        if (weeks.includes(word)) {
+          const weekIndex = weeks.indexOf(word);
 
-      console.log(num);
+          if (weekIndex === 0) {
+            const week = weeks[6];
+
+            await denops.cmd(`let @a = "${week}"`);
+            await denops.cmd('normal "_diw');
+
+            const col = await denops.call("col", ".") as number;
+            const currentLineText = await denops.call("getline", ".") as string;
+
+            if (currentLineText[col + 1] === " ") {
+              await denops.cmd('normal "ap');
+            } else {
+              await denops.cmd('normal "aP');
+            }
+          } else {
+            const week = weeks[weekIndex - 1];
+
+            await denops.cmd(`let @a = "${week}"`);
+            await denops.cmd('normal "_diw');
+
+            const col = await denops.call("col", ".") as number;
+            const currentLineText = await denops.call("getline", ".") as string;
+
+            if (currentLineText[col + 1] === " ") {
+              await denops.cmd('normal "ap');
+            } else {
+              await denops.cmd('normal "aP');
+            }
+          }
+        }
+      } else {
+        --num;
+        write(denops, num);
+      }
     },
   };
 
