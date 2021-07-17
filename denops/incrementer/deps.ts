e2d56c4fcb2324e46fa53b378cfd3a7e0c36d2fc
0ta2 <mangamania5643+github@gmail.com>
2021-07-17 23:44:44 +0900

    refactoring

--- denops/incrementer/deps.ts
+++ denops/incrementer/deps.ts
@@ -1,3 +1,4 @@
 export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.1/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v1.0.0-beta.1/function/mod.ts";
 export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.1/helper/mod.ts";
+export { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
--- denops/incrementer/main.ts
+++ denops/incrementer/main.ts
@@ -1,4 +1,5 @@
 import type { Denops } from "./deps.ts";
+import { ensureString } from "./deps.ts";
 import { execute } from "./deps.ts";
 import { cword } from "./cword.ts";
 import { write } from "./write.ts";
@@ -7,38 +8,32 @@ import { incOrDec } from "./incOrDec.ts";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
-    async increment(): Promise<void> {
+    async incrementer(direction: unknown): Promise<void> {
       const word: string = await cword(denops);
       let num = Number(word);
 
       if (isNaN(num)) {
-        const inc = incOrDec(weeks, word, "increment");
+        ensureString(direction);
+        const inc = incOrDec(weeks, word, direction);
         write(denops, inc);
       } else {
-        ++num;
-        write(denops, num);
-      }
-    },
-
-    async decrement(): Promise<void> {
-      const word: string = await cword(denops);
-      let num = Number(word);
-
-      if (isNaN(num)) {
-        const inc = incOrDec(weeks, word, "decrement");
-        write(denops, inc);
-      } else {
-        --num;
-        write(denops, num);
+        if (direction === "increment") {
+          ++num;
+          write(denops, num);
+        } else {
+          --num;
+          write(denops, num);
+        }
       }
     },
   };
 
+  // 16/15(日)
+  //
   await execute(
     denops,
     `
-    command! Incrementer echomsg denops#request('${denops.name}', 'increment', [])
-    command! Decrement echomsg denops#request('${denops.name}', 'decrement', [])
+    command! -nargs=1 Incrementer echomsg denops#request('${denops.name}', 'incrementer', [<q-args>])
     `,
   );
 }
