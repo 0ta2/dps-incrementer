c52be44275cb42401535c6535e9d5aaa7fe49c8e
0ta2 <mangamania5643+github@gmail.com>
2021-08-01 15:43:41 +0900

    01とかの数字も対応してみた

--- denops/incrementer/main.ts
+++ denops/incrementer/main.ts
@@ -1,6 +1,5 @@
 import type { Denops } from "./deps.ts";
-import { ensureString } from "./deps.ts";
-import { execute } from "./deps.ts";
+import { ensureString, execute, util } from "./deps.ts";
 import { cword } from "./cword.ts";
 import { write } from "./write.ts";
 import { weeks } from "./values/weeks.ts";
@@ -10,13 +9,28 @@ export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     async incrementer(direction: unknown): Promise<void> {
       const word: string = await cword(denops);
-      let num = Number(word);
 
-      if (isNaN(num)) {
-        ensureString(direction);
-        const inc = incOrDec(weeks, word, direction);
-        write(denops, inc);
-      } else {
+      const reg = /^(0+)/;
+
+      // A number with a zero at the beginning.
+      if (reg.test(word)) {
+        const regWord = reg.exec(word);
+        if (regWord !== null) {
+          let num = Number(word);
+          if (direction === "increment") {
+            ++num;
+            write(denops, regWord[1] + num);
+          } else {
+            --num;
+            write(denops, regWord[1] + num);
+          }
+        }
+        return;
+      }
+
+      // Number
+      if (util.isNumeric(word)) {
+        let num = Number(word);
         if (direction === "increment") {
           ++num;
           write(denops, num);
@@ -24,6 +38,16 @@ export async function main(denops: Denops): Promise<void> {
           --num;
           write(denops, num);
         }
+
+        return;
+      }
+
+      // Week
+      if (weeks.includes(word)) {
+        ensureString(direction);
+        const inc = incOrDec(weeks, word, direction);
+        write(denops, inc);
+        return;
       }
     },
   };
